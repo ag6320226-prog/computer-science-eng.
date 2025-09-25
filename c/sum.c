d9e85c37de2b7e26f1bbe056f80c3e65e0061a1f
Ayush Gupta <ag6320226@gmail.com>
2025-09-25 15:27:23 +0530

    Add files via upload

---
+++ c/sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    printf("Enter 1st number :");
+    scanf("%d",&a);
+    int b;
+    printf("Enter 2nd number :");
+    scanf("%d",&b);
+    int sum =  a + b;
+    printf("%d",sum);
+    return 0;
+    
+}
