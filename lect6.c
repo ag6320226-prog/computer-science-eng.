683cd11932eb0fba770b30b6cab72c3844f31294
Ayush Gupta <ag6320226@gmail.com>
2025-10-07 18:46:34 +0530

    prime

---
+++ lect6.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+int main()
+{
+//     // int n = 6;
+//     // int i;
+//     // if (n <= 1)
+//     // {
+//     //     printf("not prime\n");
+//     // }
+//     // for (i = 2; i < n; i++)
+//     // {
+//     //     if (n % i == 0)
+//     //     {
+//     //         printf("ye nhi haii\n");
+//     //     }
+//     //     else
+//     //     {
+
+//     //         printf("primee haiiii");
+//     //     }
+//     // }
+
+//     // int num;
+//     // scanf("%d",&num);
+//     //   int original=num;
+//     // int res=0;
+//     // while (num!=0)
+//     // {
+//     //     int lastDigit=num%10;
+//     //     res=  res*10+  lastDigit;
+//     //     num=num/10;
+
+//     // }
+//     // if(original==res){
+//     //     printf("p");
+//     // }else{
+//     //     printf("not");
+//     // }
+
+//     // int i , n , fact=1;
+//     // printf("number:");
+//     // scanf("%d",&n);
+//     // for (i=1;i<=n;i++)
+    
+//     // {
+//     //     fact=fact*i;
+//     // }
+//     // printf("Factorial=%d",fact);
+
+
+//     // int num=121;
+//     // int original=num;
+//     // int res=0;
+//     // while (num!=0)
+//     // {
+//     //    int lastD=   num%10;
+//     //    res=  res*10+  lastD;
+//     //   num=   num/10;
+        
+//     // }
+//     // if(original==res){
+//     //     printf("palindronnnnnnn");
+//     // }else{
+//     //     printf("not ");
+//     // }
+
+
+
+
+// for (int i = 1; i <= 5; i++) {
+//     if (i == 3) break;;   // i = 3 pe skip
+//     printf("%d ", i);
+// }
+// for (int i = 1; i <= 5; i++) {
+//     if (i == 3) continue;;;   // i = 3 pe skip
+//     printf("%d ", i);
+// }
+
+    
+// }
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n = 6;
+//     int i;
+//     if (n<=1)
+//     {
+//         printf("not prime\n");
+//     }
+//     for (i = 0; i < n; i++)
+//     {
+//         if (n % i==1)
+//         {
+//             printf("ye nhi haii\n");
+//         }else{
+//             printf("prime haiii");
+//         }
+//     }
+}
